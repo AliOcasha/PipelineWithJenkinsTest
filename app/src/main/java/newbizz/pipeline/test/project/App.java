fcd49b51c76640b3c4aa4e7cf718e0b3071552d1
Ali Ocasha <ali.ocasha@valtech-mobility.com>
2022-10-16 13:55:03 +0200

    Master Changes

--- app/src/main/java/newbizz/pipeline/test/project/App.java
+++ app/src/main/java/newbizz/pipeline/test/project/App.java
@@ -5,7 +5,7 @@ package newbizz.pipeline.test.project;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World in master!";
     }
 
     public static void main(String[] args) {
